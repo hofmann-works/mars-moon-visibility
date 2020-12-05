c188d0793a029e1cd433b307103c324d8fdb4897
Robert Hofmann <robert@hofmann.works>
2020-12-05 15:14:38 +0100

    add dotnet console template project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mars_moon_visibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
